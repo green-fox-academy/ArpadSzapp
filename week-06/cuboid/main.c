29378ffc3d951e8c5a5898998ef13aebe8123e68
ArpadSzapp <arpad.szapp@gmail.com>
2019-02-25 16:01:33 +0100

    cuboidSurfVol

---
+++ week-06/cuboid/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main ()
+{
+
+// Write a program that stores 3 sides of a cuboid as variables (doubles)
+double sideL;
+double sideW;
+double sideH;
+// You should get these variables via console input
+printf("type in the 3 side of a cuboid\n");
+scanf("%lf %lf %lf", &sideL, &sideW, &sideH);
+
+// The program should write the surface area and volume of the cuboid like:
+printf("Surface Area: %lf\nVolume:%lf", (2*(sideL*sideW+sideL*sideH+sideW*sideH)), (sideL*sideW*sideH));
+//
+// Surface Area: 600
+// Volume: 1000
+    return 0;
+}
