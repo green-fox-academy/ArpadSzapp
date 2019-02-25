cd070e6187659b27264247f9993477ab41b502af
ArpadSzapp <arpad.szapp@gmail.com>
2019-02-25 15:18:55 +0100

    addDetails

---
+++ week-06/introduce/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main ()
+{
+    char *name = "Arpad";
+    int age = 25;
+    int height = 175;
+
+    printf("%s \n", name);
+    printf( "%d \n", age);
+    printf("%d", height);
+
+    return 0;
+}
