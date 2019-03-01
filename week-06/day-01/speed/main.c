04add11639a2fc68cea35dea0b464b49d6167642
ArpadSzapp <arpad.szapp@gmail.com>
2019-03-01 13:23:04 +0100

    typecasting speed

--- week-06/day-01/speed/main.c
+++ week-06/day-01/speed/main.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 #define DISTANCE 345
-
+#define speed DISTANCE / seconds
 // fix the program so it prints out the correct result!
 
 int main()
 {
     int seconds = 17;
-    float speed = DISTANCE / seconds;
 
-    printf("%f", speed);
+    printf("%f", (float)speed);
 
     return 0;
 }
