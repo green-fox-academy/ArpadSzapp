cd74e488fd3c0d4222a5ab1bb6f2be66df6349b6
ArpadSzapp <arpad.szapp@gmail.com>
2019-03-01 15:29:37 +0100

    use strcpy

--- week-06/day-02/sandwich/main.c
+++ week-06/day-02/sandwich/main.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 /*
 Create a sandwich struct
 It should store:
@@ -11,28 +13,38 @@ The parameters should be:
  - and an integer that represents how many sandwiches you want to order
 */
 
+
 typedef struct
 {
-    char *_name;
+    char _name[30];
     float _price;
     float _weight;
 
 } Sandwich;
 
+float priceOfSandwich(Sandwich sandwich, int number_of_sandwich);
+
 int main ()
 {
     Sandwich chicken1;
-    chicken1._name = "Chicken sandwich with tomato";
+    strcpy(chicken1._name, "Chicken sandwich with tomato");
     chicken1._price = 21.70;
     chicken1._weight = 550;
 
     Sandwich chicken2;
-    chicken2._name = "Chicken sandwich with hot pepper and greek salad";
+    strcpy(chicken2._name, "Chicken sandwich with hot pepper and greek salad");
     chicken2._price = 50.90;
     chicken2._weight = 460;
 
-    printf("\n%s, theprice is: %.2f$, with awesome weight %.2fgramms", chicken1._name, chicken1._price, chicken1._weight);
-    printf("\n%s, theprice is: %.2f$, with awesome weight %.2fgramms", chicken2._name, chicken2._price, chicken2._weight);
-
+    printf("\nYour sandwich(es) price is: %.2f $", priceOfSandwich(chicken1, 4));
+    printf("\nYour sandwich(es) price is: %.2f $", priceOfSandwich(chicken2, 3));
     return 0;
+}
+
+float priceOfSandwich(Sandwich sandwich, int number_of_sandwich)
+{
+    float price;
+
+    price = sandwich._price * number_of_sandwich;
+    return price;
 }
