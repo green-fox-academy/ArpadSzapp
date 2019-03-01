d40524219ff9667df47ad382491f1ef492df7e42
ArpadSzapp <arpad.szapp@gmail.com>
2019-03-01 14:51:46 +0100

    add values to the fields of structs

--- week-06/day-02/computer/main.c
+++ week-06/day-02/computer/main.c
@@ -6,7 +6,7 @@
 
 struct Computer
 {
-    char* name;
+    char *name;
     float cpu_speed_GHz;
     int ram_size_GB;
     int bits;
@@ -22,19 +22,22 @@ typedef struct
 
 int main ()
 {
+    //give values to computer struct
     struct Computer homePC;
-    homePC.name="Granma is awesome";
+    homePC.name = "Granma is awesome";
     homePC.bits = 32;
     homePC.cpu_speed_GHz = 1.4;
     homePC.ram_size_GB = 8;
 
+    //give values to computer struct
     struct Computer gamerPC;
-    gamerPC.name="My son can't make a sandwich";
+    gamerPC.name = "My son can't make a sandwich";
     gamerPC.bits = 64;
     gamerPC.cpu_speed_GHz = 4.5;
     gamerPC.ram_size_GB = 32;
 
 
+    //give values for struct notebook
     Notebook ACER1;
     ACER1.name = " ACER SWIFT 1 SF113-31-P3BY ";
     ACER1.ram_size_GB = 4;
@@ -45,7 +48,7 @@ int main ()
            gamerPC.name, gamerPC.cpu_speed_GHz, gamerPC.ram_size_GB, gamerPC.bits);
 
     printf("\nYour Computer name is: %s speed is: %.3f (GHz), ram size is: %d GB, and the operating system type is %d bits\n",
-           homePC.name,homePC.cpu_speed_GHz, homePC.ram_size_GB, homePC.bits);
+           homePC.name, homePC.cpu_speed_GHz, homePC.ram_size_GB, homePC.bits);
 
     printf("\nYour Notebook type is: %s, the speed is: %.3f (GHz), ram size is: %d GB, and the operating system type is %d bits\n",
            ACER1.name, ACER1.cpu_speed_GHz, ACER1.ram_size_GB, ACER1.bits);
