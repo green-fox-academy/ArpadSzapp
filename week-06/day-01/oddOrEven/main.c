93b5c876f142d91201910e4d850dcf3ff0d8fe94
ArpadSzapp <arpad.szapp@gmail.com>
2019-03-01 11:10:39 +0100

    return (num%2==0) odd or not

---
+++ week-06/day-01/oddOrEven/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int oddSelect (int num);
+int main ()
+{
+    int num;
+    printf("write in the num, to know odd or not:");
+    scanf("%d", &num);
+    printf("%d",oddSelect(num));
+
+    return 0;
+}
+
+int oddSelect (int num)
+{
+    return (num%2==0);
+}
