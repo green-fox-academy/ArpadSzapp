e96818e55dc51df83189007213d0533ab8dc1203
ArpadSzapp <arpad.szapp@gmail.com>
2019-03-01 13:28:00 +0100

    start fix

---
+++ week-06/day-01/position/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+// create a function which takes a char array as a parameter and
+// lists all position where character 'i' is found
+
+int main ()
+{
+	char string[55] = "This is a string for testing";
+	char *p;
+
+	return 0;
+}
