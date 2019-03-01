7c2194e0a6a3a5092863ca59d2ca63b54275b51a
ArpadSzapp <arpad.szapp@gmail.com>
2019-03-01 14:07:10 +0100

    list all position of a character in a string

--- week-06/day-01/position/main.c
+++ week-06/day-01/position/main.c
@@ -4,10 +4,23 @@
 // create a function which takes a char array as a parameter and
 // lists all position where character 'i' is found
 
+void listCharPos (char *sentence, char iChar);
+
 int main ()
 {
-	char string[55] = "This is a string for testing";
-	char *p;
+    char string[55] = "This is a string for testing";
+    char iChar = 'i';
+    //char *p;
+    listCharPos(string, iChar);
 
-	return 0;
+    return 0;
+}
+
+void listCharPos (char *sentence, char iChar)
+{
+    for (int i = 0; i < strlen(sentence); ++i) {
+        if (sentence[i] == iChar) {
+            printf("%d\n", i);
+        }
+    }
 }
