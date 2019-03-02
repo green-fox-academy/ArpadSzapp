49da480cd50845fc99e0ff696520ce3242932a94
ArpadSzapp <arpad.szapp@gmail.com>
2019-03-02 17:23:07 +0100

    start add next step

--- week-06/day-02/smartphones/main.c
+++ week-06/day-02/smartphones/main.c
@@ -1,4 +1,16 @@
 #include <stdio.h>
+/*
+Create a smartphone register application
+* Read the content of smartphones.txt and store the informations in a structure called "smartphone:
+* - the name of the gadget (which is shorter than 256 characters) (Don't bother with the underscore('_') character, it's the part of the name)
+* - the year of the release (e.g. 2016)
+* - the type of the screen size (as a custom type, see below)
+*
+* In the file the size is given in cm but you should store the screen size type in an enumeration ("screen_size"),
+* the valid types are:
+*  - BIG -> (>= 15 cm)
+*  - MEDIUM -> (>= 12 cm)
+*  - SMALL -> (< 12 cm)*/
 
 typedef enum
 {
@@ -16,6 +28,6 @@ typedef struct
 
 int main ()
 {
-    printf("Hello, World!\n");
+    
     return 0;
 }
