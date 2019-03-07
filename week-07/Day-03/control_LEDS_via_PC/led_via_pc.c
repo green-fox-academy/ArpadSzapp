f821b2560af8d0bb0a3ed5b5bfd3785d642fd0dc
ArpadSzapp <arpad.szapp@gmail.com>
2019-03-07 17:43:14 +0100

    use_LED_Via_PC

---
+++ week-07/Day-03/control_LEDS_via_PC/led_via_pc.c
@@ -0,0 +1,119 @@
+#include "stm32f7xx.h"
+#include "stm32746g_discovery.h"
+#include <string.h>
+
+#define UART_PUTCHAR int __io_putchar(int ch)
+
+GPIO_InitTypeDef GPIOTxConfig;
+GPIO_InitTypeDef GPIORxConfig;
+GPIO_InitTypeDef LEDS;
+UART_HandleTypeDef UartHandle;
+
+int main(void) {
+	HAL_Init();
+
+	/* we need to enable the GPIOA port's clock first */
+	__HAL_RCC_GPIOF_CLK_ENABLE()
+	;
+
+	LEDS.Pin = GPIO_PIN_9 | GPIO_PIN_10; /* setting up 2 pins at once with | operator */
+	LEDS.Mode = GPIO_MODE_OUTPUT_PP; /* configure as output, in push-pull mode */
+	LEDS.Pull = GPIO_NOPULL; /* we don't need internal pull-up or -down resistor */
+	LEDS.Speed = GPIO_SPEED_HIGH; /* we need a high-speed output */
+
+	HAL_GPIO_Init(GPIOF, &LEDS); /* initialize the pin on GPIOF port */
+
+	/* enable GPIO clock */
+	__HAL_RCC_GPIOA_CLK_ENABLE()
+	;
+	__HAL_RCC_GPIOB_CLK_ENABLE()
+	;
+
+	/* configure GPIO for UART transmit line */
+	GPIOTxConfig.Pin = GPIO_PIN_9;
+	GPIOTxConfig.Mode = GPIO_MODE_AF_PP;
+	GPIOTxConfig.Pull = GPIO_NOPULL;
+	GPIOTxConfig.Speed = GPIO_SPEED_FAST;
+	GPIOTxConfig.Alternate = GPIO_AF7_USART1;
+
+	HAL_GPIO_Init(GPIOA, &GPIOTxConfig);
+
+	/* configure GPIO for UART receive line */
+	GPIORxConfig.Pin = GPIO_PIN_7;
+	GPIORxConfig.Mode = GPIO_MODE_AF_PP;
+	GPIOTxConfig.Pull = GPIO_NOPULL;
+	GPIORxConfig.Speed = GPIO_SPEED_FAST;
+	GPIORxConfig.Alternate = GPIO_AF7_USART1;
+
+	HAL_GPIO_Init(GPIOB, &GPIORxConfig);
+
+	/* enable the clock of the used peripherial instance */
+	__HAL_RCC_USART1_CLK_ENABLE()
+	;
+
+	/* defining the UART configuration structure */
+	UartHandle.Instance = USART1;
+	UartHandle.Init.BaudRate = 115200;
+	UartHandle.Init.WordLength = UART_WORDLENGTH_8B;
+	UartHandle.Init.StopBits = UART_STOPBITS_1;
+	UartHandle.Init.Parity = UART_PARITY_NONE;
+	UartHandle.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+	UartHandle.Init.Mode = UART_MODE_TX_RX;
+
+	HAL_UART_Init(&UartHandle);
+
+	__HAL_RCC_GPIOF_CLK_ENABLE()
+	;
+
+	LEDS.Pin = GPIO_PIN_9 | GPIO_PIN_10; /* setting up 2 pins at once with | operator */
+	LEDS.Mode = GPIO_MODE_OUTPUT_PP; /* configure as output, in push-pull mode */
+	LEDS.Pull = GPIO_NOPULL; /* we don't need internal pull-up or -down resistor */
+	LEDS.Speed = GPIO_SPEED_HIGH; /* we need a high-speed output */
+
+	HAL_GPIO_Init(GPIOF, &LEDS); /* initialize the pin on GPIOF port */
+
+	char hello[10];
+
+	while (1) {
+
+		memset(hello, '\0', 10);
+
+		HAL_UART_Receive(&UartHandle, (uint8_t*) hello, sizeof(hello), 500);
+		printf("%s", hello);
+
+		if (strcmp(hello, "ON") == 0) {
+
+			printf("%s", "\nelsoprint");
+
+			HAL_GPIO_WritePin(GPIOF, GPIO_PIN_9, GPIO_PIN_SET); /* setting the pin to 1 */
+			HAL_GPIO_WritePin(GPIOF, GPIO_PIN_10, GPIO_PIN_SET); /* setting the pin to 1 */
+			printf("%s", "\nLED_IS_ON");
+
+		} else if (strcmp(hello, "OFF") == 0) {
+
+			HAL_GPIO_WritePin(GPIOF, GPIO_PIN_9, GPIO_PIN_RESET); /* setting the pin to 0 */
+			HAL_GPIO_WritePin(GPIOF, GPIO_PIN_10, GPIO_PIN_RESET); /* setting the pin to 0 */
+
+			printf("%s", "LED_IS_OFF");
+
+		} else if (strcmp(hello, "") == 0) {
+		} else {
+
+			for (int i = 0; i < 3; ++i) {
+				HAL_GPIO_WritePin(GPIOF, GPIO_PIN_9, GPIO_PIN_SET); /* setting the pin to 0 */
+				HAL_GPIO_WritePin(GPIOF, GPIO_PIN_10, GPIO_PIN_SET); /* setting the pin to 0 */
+				HAL_Delay(250);
+				HAL_GPIO_WritePin(GPIOF, GPIO_PIN_9, GPIO_PIN_RESET); /* setting the pin to 0 */
+				HAL_GPIO_WritePin(GPIOF, GPIO_PIN_10, GPIO_PIN_RESET); /* setting the pin to 0 */
+				HAL_Delay(250);
+			}
+		}
+
+	}
+
+}
+
+UART_PUTCHAR {
+	HAL_UART_Transmit(&UartHandle, (uint8_t*) &ch, 1, 0xFFFF);
+	return ch;
+}
