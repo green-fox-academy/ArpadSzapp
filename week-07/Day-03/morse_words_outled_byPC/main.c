fa06ce44d3e186882834df541680d212fe958bc4
ArpadSzapp <arpad.szapp@gmail.com>
2019-03-08 15:01:35 +0100

    add_few_more_led

--- week-07/Day-03/morse_words_outled_byPC/main.c
+++ week-07/Day-03/morse_words_outled_byPC/main.c
@@ -18,7 +18,7 @@ int main(void) {
 	__HAL_RCC_GPIOF_CLK_ENABLE()
 	;
 
-	LEDS.Pin = GPIO_PIN_9 | GPIO_PIN_10; /* setting up 2 pins at once with | operator */
+	LEDS.Pin = GPIO_PIN_7 | GPIO_PIN_8 | GPIO_PIN_9 | GPIO_PIN_10; /* setting up 2 pins at once with | operator */
 	LEDS.Mode = GPIO_MODE_OUTPUT_PP; /* configure as output, in push-pull mode */
 	LEDS.Pull = GPIO_NOPULL; /* we don't need internal pull-up or -down resistor */
 	LEDS.Speed = GPIO_SPEED_HIGH; /* we need a high-speed output */
@@ -175,20 +175,32 @@ int main(void) {
 
 void short_morse(int n) {
 	for (int i = 0; i < n; ++i) {
+		HAL_GPIO_WritePin(GPIOF, GPIO_PIN_7, GPIO_PIN_SET); /* setting the pin to 1 */
+		HAL_GPIO_WritePin(GPIOF, GPIO_PIN_8, GPIO_PIN_SET); /* setting the pin to 1 */
 		HAL_GPIO_WritePin(GPIOF, GPIO_PIN_9, GPIO_PIN_SET); /* setting the pin to 1 */
+		HAL_GPIO_WritePin(GPIOF, GPIO_PIN_10, GPIO_PIN_SET); /* setting the pin to 1 */
 		HAL_Delay(250);
-		HAL_GPIO_WritePin(GPIOF, GPIO_PIN_9, GPIO_PIN_RESET); /* setting the pin to 1 */
+		HAL_GPIO_WritePin(GPIOF, GPIO_PIN_7, GPIO_PIN_RESET); /* setting the pin to 1 */
+				HAL_GPIO_WritePin(GPIOF, GPIO_PIN_8, GPIO_PIN_RESET); /* setting the pin to 1 */
+				HAL_GPIO_WritePin(GPIOF, GPIO_PIN_9, GPIO_PIN_RESET); /* setting the pin to 1 */
+				HAL_GPIO_WritePin(GPIOF, GPIO_PIN_10, GPIO_PIN_RESET); /* setting the pin to 1 */
 		HAL_Delay(250);
-	}
+	}HAL_Delay(500);
 }
 
 void long_morse(int n) {
 	for (int i = 0; i < n; ++i) {
-		HAL_GPIO_WritePin(GPIOF, GPIO_PIN_9, GPIO_PIN_SET); /* setting the pin to 1 */
-		HAL_Delay(750);
-		HAL_GPIO_WritePin(GPIOF, GPIO_PIN_9, GPIO_PIN_RESET); /* setting the pin to 1 */
-		HAL_Delay(750);
-	}
+		HAL_GPIO_WritePin(GPIOF, GPIO_PIN_7, GPIO_PIN_SET); /* setting the pin to 1 */
+				HAL_GPIO_WritePin(GPIOF, GPIO_PIN_8, GPIO_PIN_SET); /* setting the pin to 1 */
+				HAL_GPIO_WritePin(GPIOF, GPIO_PIN_9, GPIO_PIN_SET); /* setting the pin to 1 */
+				HAL_GPIO_WritePin(GPIOF, GPIO_PIN_10, GPIO_PIN_SET); /* setting the pin to 1 */
+				HAL_Delay(750);
+				HAL_GPIO_WritePin(GPIOF, GPIO_PIN_7, GPIO_PIN_RESET); /* setting the pin to 1 */
+						HAL_GPIO_WritePin(GPIOF, GPIO_PIN_8, GPIO_PIN_RESET); /* setting the pin to 1 */
+						HAL_GPIO_WritePin(GPIOF, GPIO_PIN_9, GPIO_PIN_RESET); /* setting the pin to 1 */
+						HAL_GPIO_WritePin(GPIOF, GPIO_PIN_10, GPIO_PIN_RESET); /* setting the pin to 1 */
+				HAL_Delay(750);
+	}HAL_Delay(250);
 }
 UART_PUTCHAR {
 	HAL_UART_Transmit(&UartHandle, (uint8_t*) &ch, 1, 0xFFFF);
