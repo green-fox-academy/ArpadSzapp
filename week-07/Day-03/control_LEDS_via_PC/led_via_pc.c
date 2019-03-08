642ce4b9ecde126187bf12ae42782ea6b967d605
ArpadSzapp <arpad.szapp@gmail.com>
2019-03-08 11:40:43 +0100

    use_some_void

--- week-07/Day-03/control_LEDS_via_PC/led_via_pc.c
+++ week-07/Day-03/control_LEDS_via_PC/led_via_pc.c
@@ -11,6 +11,136 @@ UART_HandleTypeDef UartHandle;
 
 int main(void) {
 	HAL_Init();
+#include "stm32f7xx.h"
+#include "stm32746g_discovery.h"
+#include <string.h>
+
+#define UART_PUTCHAR int __io_putchar(int ch)
+
+GPIO_InitTypeDef GPIOTxConfig;
+GPIO_InitTypeDef GPIORxConfig;
+GPIO_InitTypeDef LEDS;
+UART_HandleTypeDef UartHandle;
+
+void led_ON_state();
+void led_OFF_state();
+void error_flash(int n);
+int main(void) {
+	HAL_Init();
+
+	/* we need to enable the GPIOA port's clock first */
+	__HAL_RCC_GPIOF_CLK_ENABLE()
+	;
+
+	LEDS.Pin = GPIO_PIN_9 | GPIO_PIN_10; /* setting up 2 pins at once with | operator */
+	LEDS.Mode = GPIO_MODE_OUTPUT_PP; /* configure as output, in push-pull mode */
+	LEDS.Pull = GPIO_NOPULL; /* we don't need internal pull-up or -down resistor */
+	LEDS.Speed = GPIO_SPEED_HIGH; /* we need a high-speed output */
+
+	HAL_GPIO_Init(GPIOF, &LEDS); /* initialize the pin on GPIOF port */
+
+	/* enable GPIO clock */
+	__HAL_RCC_GPIOA_CLK_ENABLE()
+	;
+	__HAL_RCC_GPIOB_CLK_ENABLE()
+	;
+
+	/* configure GPIO for UART transmit line */
+	GPIOTxConfig.Pin = GPIO_PIN_9;
+	GPIOTxConfig.Mode = GPIO_MODE_AF_PP;
+	GPIOTxConfig.Pull = GPIO_NOPULL;
+	GPIOTxConfig.Speed = GPIO_SPEED_FAST;
+	GPIOTxConfig.Alternate = GPIO_AF7_USART1;
+
+	HAL_GPIO_Init(GPIOA, &GPIOTxConfig);
+
+	/* configure GPIO for UART receive line */
+	GPIORxConfig.Pin = GPIO_PIN_7;
+	GPIORxConfig.Mode = GPIO_MODE_AF_PP;
+	GPIOTxConfig.Pull = GPIO_NOPULL;
+	GPIORxConfig.Speed = GPIO_SPEED_FAST;
+	GPIORxConfig.Alternate = GPIO_AF7_USART1;
+
+	HAL_GPIO_Init(GPIOB, &GPIORxConfig);
+
+	/* enable the clock of the used peripherial instance */
+	__HAL_RCC_USART1_CLK_ENABLE()
+	;
+
+	/* defining the UART configuration structure */
+	UartHandle.Instance = USART1;
+	UartHandle.Init.BaudRate = 115200;
+	UartHandle.Init.WordLength = UART_WORDLENGTH_8B;
+	UartHandle.Init.StopBits = UART_STOPBITS_1;
+	UartHandle.Init.Parity = UART_PARITY_NONE;
+	UartHandle.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+	UartHandle.Init.Mode = UART_MODE_TX_RX;
+
+	HAL_UART_Init(&UartHandle);
+
+	__HAL_RCC_GPIOF_CLK_ENABLE()
+	;
+
+	LEDS.Pin = GPIO_PIN_9 | GPIO_PIN_10 | GPIO_PIN_7 | GPIO_PIN_8; /* setting up 2 pins at once with | operator */
+	LEDS.Mode = GPIO_MODE_OUTPUT_PP; /* configure as output, in push-pull mode */
+	LEDS.Pull = GPIO_NOPULL; /* we don't need internal pull-up or -down resistor */
+	LEDS.Speed = GPIO_SPEED_HIGH; /* we need a high-speed output */
+
+	HAL_GPIO_Init(GPIOF, &LEDS); /* initialize the pin on GPIOF port */
+
+	char words[10];
+	memset(words, '\0', 10);
+
+	while (1) {
+
+		memset(words, '\0', 10);
+
+		HAL_UART_Receive(&UartHandle, (uint8_t*) words, sizeof(words), 500);
+
+		if (strcmp(words, "ON") == 0) {
+
+					led_ON_state();
+
+		} else if (strcmp(words, "OFF") == 0) {
+
+			led_OFF_state();
+
+		} else if (strcmp(words, "") == 0) {
+
+		} else {
+
+			error_flash(3);
+			memset(words, '\0', 10);
+		}
+	}
+}
+
+void led_ON_state() {
+	HAL_GPIO_WritePin(GPIOF, GPIO_PIN_7, GPIO_PIN_SET); /* setting the pin to 0 */
+	HAL_GPIO_WritePin(GPIOF, GPIO_PIN_8, GPIO_PIN_SET); /* setting the pin to 0 */
+	HAL_GPIO_WritePin(GPIOF, GPIO_PIN_9, GPIO_PIN_SET); /* setting the pin to 0 */
+	HAL_GPIO_WritePin(GPIOF, GPIO_PIN_10, GPIO_PIN_SET); /* setting the pin to 0 */
+}
+
+void led_OFF_state() {
+	HAL_GPIO_WritePin(GPIOF, GPIO_PIN_7, GPIO_PIN_RESET); /* setting the pin to 0 */
+	HAL_GPIO_WritePin(GPIOF, GPIO_PIN_8, GPIO_PIN_RESET); /* setting the pin to 0 */
+	HAL_GPIO_WritePin(GPIOF, GPIO_PIN_9, GPIO_PIN_RESET); /* setting the pin to 0 */
+	HAL_GPIO_WritePin(GPIOF, GPIO_PIN_10, GPIO_PIN_RESET); /* setting the pin to 0 */
+}
+void error_flash(int n) {
+	for (int i = 0; i < n; ++i) {
+		led_ON_state();
+		HAL_Delay(250);
+		led_OFF_state();
+		HAL_Delay(250);
+	}
+}
+
+UART_PUTCHAR {
+	HAL_UART_Transmit(&UartHandle, (uint8_t*) &ch, 1, 0xFFFF);
+	return ch;
+}
 
 	/* we need to enable the GPIOA port's clock first */
 	__HAL_RCC_GPIOF_CLK_ENABLE()
