d984fee981ff2a26878d021110231495f158b79f
ArpadSzapp <arpad.szapp@gmail.com>
2019-03-14 14:53:17 +0100

    uart_added

--- week-08/Day-04/Temp_sensor_delay_to_timer/Interrupt_with_DELAY_to_TIMER.c
+++ week-08/Day-04/Temp_sensor_delay_to_timer/Interrupt_with_DELAY_to_TIMER.c
@@ -1,17 +1,27 @@
 #include "stm32f7xx.h"
 #include "stm32746g_discovery.h"
 
-I2C_HandleTypeDef I2C_handle;            // defining the I2C configuration structure
+#define UART_PUTCHAR int __io_putchar(int ch)
+
+I2C_HandleTypeDef I2C_handle;            	// defining the I2C configuration structure
 
 GPIO_InitTypeDef SCL_SDA_config;           // configure GPIOs for I2C data and clock lines
+
+UART_HandleTypeDef uart_handle;
+
+void init_uart();
 
 /* 7 bit address of an I2C sensor shifted to left 1 bit, leaving place for W/R bit,
  * which is inserted by the HAL_I2C_Master_Transmit and HAL_I2C_Master_Receive */
 #define TEMP_SENSOR_ADDRESS (0b1001000 << 1)
 
+
+
 int main(void)
 {
     HAL_Init();
+
+    init_uart();
 
     __HAL_RCC_GPIOB_CLK_ENABLE();       /* enable GPIO clock */
 
@@ -44,6 +54,35 @@ int main(void)
 		&read_temp_val, sizeof(read_temp_val), 100);
         */
 
-        printf("Temperature: %d", read_temp_val);
+        printf("Temperature: %d\n", read_temp_val);
     }
+}
+
+void init_uart()
+														// Initializing UART communication
+{
+	__HAL_RCC_USART1_CLK_ENABLE();
+														// Enable clock for the USART1
+	uart_handle.Instance = USART1;
+														// set USART1 register
+	uart_handle.Init.BaudRate = 115200;
+	uart_handle.Init.WordLength = UART_WORDLENGTH_8B;
+														// set 8 bit word length
+	uart_handle.Init.StopBits = UART_STOPBITS_1;
+	uart_handle.Init.Parity = UART_PARITY_NONE;
+	uart_handle.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+	uart_handle.Init.Mode = UART_MODE_TX_RX;
+
+	BSP_COM_Init(COM1, &uart_handle);
+														// initializing COM1 line for USB UART emulation line (Hercules)
+	HAL_NVIC_SetPriority(USART1_IRQn, 2, 0);
+														// set priority for USART1, preempt priority 1, sub priority 0
+	HAL_NVIC_EnableIRQ(USART1_IRQn);
+														// enable interrupt for USART1
+}
+
+UART_PUTCHAR
+{
+	HAL_UART_Transmit(&uart_handle, (uint8_t*)&ch, 1, 0xFFFF);
+	return ch;
 }
